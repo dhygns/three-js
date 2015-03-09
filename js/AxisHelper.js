f14af8f14b7fec3e7459ae99f7384dd9037c6e1f
Aki <aki@flux.io>
2015-03-09 15:22:39 -0700

    Added LabelHelper and labels to AxisHelper

---
+++ js/AxisHelper.js
@@ -0,0 +1,50 @@
+/**
+ * @author sroucheray / http://sroucheray.org/
+ * @author mrdoob / http://mrdoob.com/
+ * @author arodic / http://akirodic.com/
+ */
+
+THREE.AxisHelper = function ( size, labelSize ) {
+
+    THREE.Object3D.call( this );
+
+    size = size || 1;
+    labelSize = labelSize || 16;
+
+    var vertices = new Float32Array( [
+        0, 0, 0,  size, 0, 0,
+        0, 0, 0,  0, size, 0,
+        0, 0, 0,  0, 0, size
+    ] );
+
+    var colors = new Float32Array( [
+        1, 0, 0,  1, 0.6, 0,
+        0, 1, 0,  0.6, 1, 0,
+        0, 0, 1,  0, 0.6, 1
+    ] );
+
+    var geometry = new THREE.BufferGeometry();
+    geometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+
+    var material = new THREE.LineBasicMaterial( { vertexColors: THREE.VertexColors } );
+
+    var axis = new THREE.Line( geometry, material, THREE.LinePieces );
+    this.add( axis );
+
+    var labelX = new THREE.LabelHelper( 'x', labelSize, 'red');
+    labelX.position.set( size * 1.05, 0, 0 );
+    axis.add( labelX );
+
+    var labelY = new THREE.LabelHelper( 'y', labelSize, 'green');
+    labelY.position.set( 0, size * 1.05, 0 );
+    axis.add( labelY );
+
+    var labelZ = new THREE.LabelHelper( 'z', labelSize, 'blue');
+    labelZ.position.set( 0, 0, size * 1.05 );
+    axis.add( labelZ );
+
+};
+
+THREE.AxisHelper.prototype = Object.create( THREE.Object3D.prototype );
+THREE.AxisHelper.prototype.constructor = THREE.AxisHelper;
